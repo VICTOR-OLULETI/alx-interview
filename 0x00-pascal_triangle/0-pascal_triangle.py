b404a3f63da6fccbf2ecbc71aa35bcd8ce4c2090
VICTOR-OLULETI <oluletiv@gmail.com>
2023-09-28 22:16:42 +0100

    generate the pascal triangle through input

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,29 +2,24 @@
 """
 0-pascal_triange
 """
-def pascal_triangle(input):
+def pascal_triangle(n):
     """
     This function takes in a number input and computes
     the pascal triangle.
     """
-    if input == 0:
+    if n == 0:
         return []
 
-    if input == 1:
-        return [[1]]
-
     result = [[1]]
-    for num in range(input - 1):
+    for num in range(n - 1):
         if num == 0:
             result.append([1, 1])
         else:
-            temp = []
-            temp.append(1)
+            new_row = [1]
             # check the previous position and add the numbers like a pascal
-            temp1 = result[num]
-            for i in range(len(temp1) - 1):
-                num2 = temp1[i] + temp1[i + 1]
-                temp.append(num2)
-            temp.append(1)
-            result.append(temp)
+            prev_row_length = result[num]
+            for i in range(len(prev_row_length) - 1):
+                new_row.append(prev_row_length[i] + prev_row_length[i + 1])
+            new_row.append(1)
+            result.append(new_row)
     return result
