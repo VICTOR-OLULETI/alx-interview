b6ba9f2ffb5dfc618d37ded3bfc2f1603972e54f
VICTOR-OLULETI <oluletiv@gmail.com>
2023-09-28 21:49:45 +0100

    implement the pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+0-pascal_triange
+"""
+def pascal_triangle(input):
+    """
+    This function takes in a number input and computes
+    the pascal triangle.
+    """
+    if input == 0:
+        return ''
+    result = [[1]]
+    #result.append([1, 1])
+    for num in range(input):
+        if num == 0:
+            result.append([1, 1])
+        else:
+            temp = []
+            temp.append(1)
+            # check the previous position and add the numbers like a pascal
+            temp1 = result[num]
+            for i in range(len(temp1) - 1):
+                num2 = temp1[i] + temp1[i + 1]
+                temp.append(num2)
+            temp.append(1)
+            result.append(temp)
+    return result
