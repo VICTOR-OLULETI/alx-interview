2be8d1bbf2164e7b4d672ebe967cc457ecdb39c0
VICTOR-OLULETI <oluletiv@gmail.com>
2023-10-06 02:48:13 +0100

    implements function that determines if all boxes can be opened

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+def canUnlockAll(boxes):
+    """
+        This function determines if all the boxes can 
+        be opened
+    """
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    queue = [0]
+    while queue:
+        box = queue[0]
+        queue.remove(box)
+        for key in boxes[box]:
+            try:
+                if not visited[key]:
+                    visited[key] = True
+                    queue.append(key)
+            except IndexError:
+                continue
+    return all(visited)
