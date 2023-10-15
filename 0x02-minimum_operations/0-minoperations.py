23bf3bbf3a98503904ca3c962c9956545cfe8964
VICTOR-OLULETI <oluletiv@gmail.com>
2023-10-15 22:48:58 +0100

    function to ensure minimum operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-    This program checks the the minimum number of 
+    This program checks the the minimum number of
     operation to get 'n' 'H'
 """
 result = {}
+
+
 def minOperations(n):
     """
         this function checks the min operations required
@@ -14,22 +16,20 @@ def minOperations(n):
     result[0] = ['H', '']
     i = 0
     count = 0
-    #for i in range(n):
     while (len(result[i][0]) < n):
-        print(result)
-        if (len(result[i][0] * 2) < n):
-            result[i + 1] = [result[i][0]* 2, result[i][0]]
+        # print(result)
+        if (len(result[i][0] * 2) <= n):
+            result[i + 1] = [result[i][0] * 2, result[i][0]]
             count += 2
         elif (len(result[i][0] + result[i][1]) < n):
             result[i + 1] = [result[i][0] + result[i][1], result[i][1]]
             count += 1
         elif (len(result[i][0] + result[i][1]) == n):
             result[i + 1] = [result[i][0] + result[i][1], result[i][1]]
-            print(result)
+            # print(result)
             count += 1
-            #break
         else:
-            #check previous entry of 1
+            # check previous entry
             j = i - 1
             count -= 1
             # check if j is a positive number then
@@ -38,4 +38,3 @@ def minOperations(n):
             i = i - 1
         i = i + 1
     return count
-minOperations(4)
